bf5226fb5107a692d045a1fda735c5ec2d28f2ab
jorgealbertowork <jorgealberto.work@gmail.com>
2025-07-09 16:38:11 -0300

    fix: changed all constants to uppercase

--- main.py
+++ main.py
@@ -4,43 +4,41 @@ import turtle
 import colorgram as cg
 
 colors = cg.extract('palette.jpg', 10)
-rgb_colors = []
+RGB_COLORS = []
 
 for color in colors:
-    # save extracted colors in the rgb_colors list
+    # save extracted colors in the RGB_COLORS list
     r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
     new_color = (r, g, b)
-    rgb_colors.append(new_color)
+    RGB_COLORS.append(new_color)
 
 
 # sets the colormode for the turtle graphics library
 turtle.colormode(255)
 
 # basic settings for the turtle object
-t = turtle.Turtle()
-t.speed("fastest")
-t.ht()
-t.penup()
-t.goto(-225, -225)
+T = turtle.Turtle()
+T.speed("fastest")
+T.ht()
+T.penup()
+T.goto(-225, -225)
 
 
 def draw_lines(n_lines, y_pos):
     for _ in range(n_lines):
-        # this updates the vertical position of the turtle
-        y_pos += 50
+        y_pos += 50  # this updates the vertical position of the turtle
 
-        for _ in range(n_lines):
-            # this draws the line of dots
-            t.dot(20, random.choice(rgb_colors))
-            t.fd(50)
+        for _ in range(n_lines):  # this draws the line of dots
+            T.dot(20, random.choice(RGB_COLORS))
+            T.fd(50)
 
         # this updates the position for the drawing of a new line
-        t.goto(-225, y_pos)
+        T.goto(-225, y_pos)
 
 
 draw_lines(10, -225)
 
-screen = turtle.Screen()
-screen.exitonclick()
+SCREEN = turtle.Screen()
+SCREEN.exitonclick()
