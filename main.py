d0a63689f6e5b36607ab00931dc498e4b330a511
jorgealbertowork <jorgealberto.work@gmail.com>
2025-07-09 00:07:18 -0300

    feat: added comments to explain important parts

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import colorgram as cg
 
 colors = cg.extract('palette.jpg', 10)
 rgb_colors = []
+
 for color in colors:
+    # save extracted colors in the rgb_colors list
     r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
@@ -13,8 +15,10 @@ for color in colors:
     rgb_colors.append(new_color)
 
 
+# sets the colormode for the turtle graphics library
 turtle.colormode(255)
 
+# basic settings for the turtle object
 t = turtle.Turtle()
 t.speed("fastest")
 t.ht()
@@ -24,10 +28,15 @@ t.goto(-225, -225)
 
 def draw_lines(n_lines, y_pos):
     for _ in range(n_lines):
+        # this updates the vertical position of the turtle
         y_pos += 50
+
         for _ in range(n_lines):
+            # this draws the line of dots
             t.dot(20, random.choice(rgb_colors))
             t.fd(50)
+
+        # this updates the position for the drawing of a new line
         t.goto(-225, y_pos)
 
 
